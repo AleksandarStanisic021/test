ccc3daacdc49b02ffc51a423dd04815b7e040df7
ciandre <a.stanisic.codealkonost@gmail.com>
2025-06-08 18:55:02 +0200

    Remove redundant header from ScipContainer component

--- components/ScipContainer.tsx
+++ components/ScipContainer.tsx
@@ -3,7 +3,6 @@ import Skip from "./Skip";
 export default function ScipContainer() {
   return (
     <div className="flex flex-col items-center justify-center gap-4">
-      <h1 className="text-2xl font-bold">Scip Container</h1>
       <p className="text-lg">This is a container for Scip related content.</p>
       <p className="text-sm text-gray-500">You can add more components here.</p>
       <Skip />
