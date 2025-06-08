002b47ebe7552ad983c87d06b2d2c934f88fb759
ciandre <a.stanisic.codealkonost@gmail.com>
2025-06-08 21:35:02 +0200

    Fetch and display skip data in ScipContainer component

--- components/ScipContainer.tsx
+++ components/ScipContainer.tsx
@@ -1,10 +1,35 @@
 import Skip from "./Skip";
 
-export default function ScipContainer() {
+export default async function ScipContainer() {
+  const res = await fetch(
+    "https://app.wewantwaste.co.uk/api/skips/by-location?postcode=NR32&area=Lowestoft"
+  );
+  const data = await res.json();
+  console.log(data);
+
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <p className="text-lg">This is a container for Scip related content.</p>
-      <Skip />
+      {data.map(
+        (skip: {
+          id: string;
+          name: string;
+          description: string;
+          price: number;
+          imageUrl: string;
+          location: string;
+        }) => (
+          <Skip
+            key={skip.id}
+            id={skip.id ? skip.id : "unknown-id"}
+            name={skip.name}
+            description={skip.description}
+            price={skip.price}
+            imageUrl={skip.imageUrl}
+            location={skip.location}
+          />
+        )
+      )}
     </div>
   );
 }
