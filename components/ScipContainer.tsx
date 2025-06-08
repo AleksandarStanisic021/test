20fc59a0d59cac82c898ea810b40cce9d52c12c6
ciandre <a.stanisic.codealkonost@gmail.com>
2025-06-08 19:12:56 +0200

    Refactor layout by adding Navbar to RootLayout and removing it from Home component; clean up ScipContainer by removing unnecessary text

--- components/ScipContainer.tsx
+++ components/ScipContainer.tsx
@@ -4,7 +4,6 @@ export default function ScipContainer() {
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <p className="text-lg">This is a container for Scip related content.</p>
-      <p className="text-sm text-gray-500">You can add more components here.</p>
       <Skip />
     </div>
   );
