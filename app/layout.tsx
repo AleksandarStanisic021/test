26b34f105cb4a784a94fee82cd44494104870d4b
ciandre <a.stanisic.codealkonost@gmail.com>
2025-06-09 16:00:52 +0200

    Wrap Navbar component in header for improved semantic structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <div>
-          <Navbar />
+          <header>
+            <Navbar />
+          </header>
         </div>
         <main>{children}</main>
       </body>
