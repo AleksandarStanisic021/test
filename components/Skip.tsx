6b7a2e2167d860560865592adb9f6be506fa93d8
ciandre <a.stanisic.codealkonost@gmail.com>
2025-06-09 19:43:47 +0200

    Refactor code structure for improved readability and maintainability

--- components/Skip.tsx
+++ components/Skip.tsx
@@ -1,12 +1,38 @@
 import { SkipProps } from "./tools/SkipProps";
+import { Badge } from "@/components/ui/badge";
+
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+import Image from "next/image";
 
 export default async function Skip(props: SkipProps) {
   return (
-    <div>
-      <h2>{props.id}</h2>
-      <p>{props.hire_period_days}</p>
-      <p>Price: £{props.price_before_vat}</p>
-      <p>Location: {props.postcode}</p>
+    <div className="flex justify-center gap-1.5 mb-4">
+      <Card>
+        <CardHeader>
+          <CardDescription>
+            <p>Hire Period: {props.hire_period_days} days</p>
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Image
+            src="/big-skip.jpg"
+            alt={`Image of skip ${props.id}`}
+            width={300}
+            height={200}
+            className="rounded-lg mb-4"
+          />
+          <p>Price (inc. VAT): £{(props.price_before_vat * 1.2).toFixed(2)}</p>
+          <p>Location: {props.postcode}</p>
+        </CardContent>
+        <CardFooter></CardFooter>
+      </Card>
     </div>
   );
 }
